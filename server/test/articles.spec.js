a89057f2edb213804b86e6d9790e4b8dae0b0286
mercury_wang <mercwang@hotmail.com>
2022-03-28 20:27:22 +0800

    test: init

---
+++ server/test/articles.spec.js
@@ -0,0 +1,18 @@
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let app = require('../app');
+
+let should = chai.should();
+chai.use(chaiHttp);
+
+describe('App', () => {
+  it('Should response status 200', (done) => {
+    chai
+      .request(app)
+      .get('/articles')
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
+});
